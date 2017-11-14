cb0bd6f657031f1ebe2f10318916499a1b431115
Maverick Tesini <mavericktesini@gmail.com>
2017-11-14 15:43:49 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,25 @@
 'use strict';
+
+class Question {
+  constructor (content, id) {
+    this.content = content;
+    this.id = id;
+
+    this.save();
+  }
+
+  save() {
+    return this.constructor._All.push(this);
+  }
+  static All() {
+    return this._All;
+  }
+
+  static Find(id){
+  return this._All[id-1];
+}
+
+}
+
+//private class variable for array of class
+Question._All = [];
